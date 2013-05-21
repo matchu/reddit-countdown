45d6aa2482bc09e6734403685d38100662035f77
Matchu <matchu1993@gmail.com>
2013-05-21 14:06:37 -0700

    silly! just update the raw markdown

--- countdown.py
+++ countdown.py
@@ -1,14 +1,8 @@
 from __future__ import division, print_function
 from ConfigParser import SafeConfigParser
 from datetime import datetime
-import praw
-
-UPDATE_START_LINE = '/* COUNTDOWN LIVE UPDATE: START */'
-UPDATE_END_LINE = '/* COUNTDOWN LIVE UPDATE: END */'
-UPDATE_STATIC_LINES = ['a[href="#days"], a[href="#hours"], a[href="#minutes"], a[href="#seconds"] {',
-                       '    color: inherit;',
-                       '    cursor: text;',
-                       '}']
+from praw import Reddit
+import re
 
 def compute_time_ago_params(target):
     countdown_delta = target - datetime.now()
@@ -19,40 +13,22 @@ def compute_time_ago_params(target):
         'seconds': countdown_delta.seconds
     }
 
-def build_live_style_content_rules(time_ago_params):
-    return ['a[href="#%s"]:after { content: "%d"; }' % (k, v)
-            for k, v in time_ago_params.iteritems()]
-
-def build_live_style_lines(time_ago_params):
-    return ([UPDATE_START_LINE] + UPDATE_STATIC_LINES +
-            build_live_style_content_rules(time_ago_params) +
-            [UPDATE_END_LINE])
-
-def build_new_stylesheet(prev_style, time_ago_params):
-    new_style_lines = []
-    current_lines_are_live = False
-    for line in prev_style.split('\n'):
-        if line == UPDATE_START_LINE:
-            current_lines_are_live = True
-            new_style_lines += build_live_style_lines(time_ago_params)
-        elif line == UPDATE_END_LINE:
-            current_lines_are_live = False
-        elif not current_lines_are_live:
-            new_style_lines.append(line)
-    return '\n'.join(new_style_lines)
-
 def update_countdown(username, password, subreddit_name, target):
     user_agent = '/r/{0} countdown bot'.format(subreddit_name)
-    reddit = praw.Reddit(user_agent)
+    reddit = Reddit(user_agent)
     reddit.login(username, password)
     
     subreddit = reddit.get_subreddit(subreddit_name)
-    stylesheet_data = subreddit.get_stylesheet()
-    new_style = build_new_stylesheet(stylesheet_data['stylesheet'],
-                                     compute_time_ago_params(target))
+    settings = subreddit.get_settings()
+    description = settings['description']
     
-    print(new_style)
-    subreddit.set_stylesheet(new_style, stylesheet_data['prevstyle'])
+    for key, value in compute_time_ago_params(target).iteritems():
+        pattern = "\\[[^\\]]*\\]\\(#{0}\\)".format(key) # replace [<anything>](#<key>)
+        repl = "[{0}](#{1})".format(value, key) # with [<value>](#<key>)
+        description = re.sub(pattern, repl, description)
+    
+    print(description)
+    subreddit.update_settings(description=description)
 
 if __name__ == '__main__':
     config_parser = SafeConfigParser()
